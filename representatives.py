d955a9a074880fe8a7064bf48a4ca3df4696d735
Daniel Porter <73040840+retropleinad@users.noreply.github.com>
2021-03-17 17:40:15 -0500

    Add files via upload

--- representatives.py
+++ representatives.py
@@ -4,6 +4,9 @@ from bs4 import BeautifulSoup
 HOUSE_LINK = "https://house.texas.gov/members/member-page/?district={0}"
 SENATE_LINK = "https://senate.texas.gov/members.php?d={0}"
 
+HOUSE_FILE = "house.txt"
+SENATE_FILE = "senate.txt"
+
 
 def scrape(link, senate=False):
     if senate:
@@ -11,12 +14,12 @@ def scrape(link, senate=False):
     else:
         members = 150
     for i in range(1, members + 1):
-        temp = scrape_one(link, i)
-        print(temp["email"])
+        entries = scrape_one(link, i)
+        output("house.txt", entries)
 
 
 def scrape_one(link, district):
-    area_code = "(512)"
+    area_code = "512"
     link = link.format(str(district))
     page = requests.get(link)
     soup = BeautifulSoup(page.content, "html.parser")
@@ -27,7 +30,7 @@ def scrape_one(link, district):
         "name": soup.find("h2").get_text(),
         "district": "District {0}".format(str(district)),
         "email": format_href(email),
-        "phone": soup.find(string=lambda text: area_code in text),
+        "phone": soup.find(string=lambda text: area_code in text).replace("\n", "", 2).replace("\r", "").strip(),
         "room": soup.find(string=lambda text: "Room" in text)
     }
     return out
@@ -38,12 +41,18 @@ def format_href(href, senate=False):
     formatted_href = str(href).split(">")[0] \
                               .split("<")[1] \
                               .split("\"")[1] \
-                              .replace(".", "", 2)
+                              .replace(".", "", 2) \
+                              .replace("\n", "")
     return house_link + formatted_href
 
 
-def output(file):
-    pass
+def output(file, data, delimiter=";"):
+    with open(file, "a") as out:
+        for key, value in data.items():
+            out.write(value)
+            if key != "room":
+                out.write(delimiter)
+        out.write("\n")
 
 
 scrape(HOUSE_LINK)
